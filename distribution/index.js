9dbc1456da0ee4bb5339ded6b8c87885bbed018e
dushyantb <dushyantbhalgami@gmail.com>
2017-03-19 14:48:21 +0530

    Basic working logging (Info, Debug and Error) with Daily file rotation

--- distribution/index.js
+++ distribution/index.js
@@ -10,61 +10,137 @@ var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
 
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Logger = function () {
-    function Logger(fileName) {
-        var ferq = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'd';
-        var size = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 0;
+    // constructor() {}
 
+    function Logger(logFileName, errorFileName, logLevel, logRotation, logSize, logJson, logTimeStamp) {
         _classCallCheck(this, Logger);
 
-        this.fileName = fileName;
+        this.logFileName = logFileName;
+        this.errorFileName = errorFileName;
 
-        if (ferq === 'd') {
-            _fs2.default.stat('input.txt', function (err, stats) {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log(stats);
-                console.log("Got file info successfully!");
-
-                // Check file type
-                console.log("isFile ? " + stats.isFile());
-                console.log("isDirectory ? " + stats.isDirectory());
-            });
-
-            _fs2.default.open(this.fileName, 'a+', function (err, fd) {
-                if (err) {
-                    return console.error(err);
-                }
-
-                _fs2.default.close(fd, function (err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-            });
-        }
+        this.logLevel = logLevel; // Info
+        this.logRotation = logRotation; // Daily
+        this.logSize = logSize; // 5 MB
+        this.logJson = logJson; // True
+        this.logTimeStamp = logTimeStamp; // True
     }
 
     _createClass(Logger, [{
-        key: 'debug',
-        value: function debug(debugString, debugData) {
-            _fs2.default.appendFile(this.fileName, "DEBUG: " + debugString + " : " + debugData + '\r\n', function (err) {
+        key: 'info',
+        value: function info(infoMessage) {
+            var logData = {};
+            logData['LEVEL'] = this.logLevel;
+            logData['MESSAGE'] = infoMessage;
+            logData['TIMESTAMP'] = (0, _moment2.default)((0, _moment2.default)(), 'M/D/YYYY');
+
+            _fs2.default.appendFile(this.logFileName, JSON.stringify(logData) + '\r\n', function (err) {
                 if (err) {
                     return console.log(err);
                 }
             });
         }
     }, {
-        key: 'info',
-        value: function info(infoString) {
-            _fs2.default.appendFile(this.fileName, "INFO: " + infoString + '\r\n', function (err) {
+        key: 'debug',
+        value: function debug(debugMessage, debugData) {
+            var logData = {};
+            logData['LEVEL'] = this.logLevel;
+            logData['MESSAGE'] = debugMessage;
+            logData['DETAILS'] = debugData;
+            logData['TIMESTAMP'] = (0, _moment2.default)((0, _moment2.default)(), 'M/D/YYYY');
+
+            _fs2.default.appendFile(this.logFileName, JSON.stringify(logData) + '\r\n', function (err) {
                 if (err) {
                     return console.log(err);
+                }
+            });
+        }
+    }, {
+        key: 'error',
+        value: function error(errorMessage, errorData) {
+            var logData = {};
+            logData['LEVEL'] = this.logLevel;
+            logData['MESSAGE'] = errorMessage;
+            logData['DETAILS'] = errorData;
+            logData['TIMESTAMP'] = (0, _moment2.default)((0, _moment2.default)(), 'M/D/YYYY');
+
+            _fs2.default.appendFile(this.errorFileName, JSON.stringify(logData) + '\r\n', function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+        }
+    }], [{
+        key: 'configure',
+        value: function configure(properties) {
+            var logLevel = properties.level || 'info'; // Info
+            var logRotation = properties.rotation || 'd'; // Daily
+            var logSize = properties.size * 1048576 || 5242880; // 5 MB
+            var logJson = properties.json || true; // True
+            var logTimeStamp = properties.timeStamp || true;
+
+            var logFileName = 'logs/app_log_current.log';
+            var errorFileName = 'logs/error_log_current.log';
+
+            if (!_fs2.default.existsSync('logs')) {
+                _fs2.default.mkdirSync('logs');
+            }
+
+            this._createLogFile(logFileName, logRotation, logSize);
+            this._createLogFile(errorFileName, logRotation, logSize);
+
+            return new Logger(logFileName, errorFileName, logLevel, logRotation, logSize, logJson, logTimeStamp);
+        }
+    }, {
+        key: '_createLogFile',
+        value: function _createLogFile(fileName, logRotation, logSize) {
+            _fs2.default.stat(fileName, function (err, stat) {
+                if (err && err.code == 'ENOENT') {
+                    _fs2.default.open(fileName, 'a+', function (err, fd) {
+                        if (err) {
+                            return console.error(err);
+                        }
+
+                        _fs2.default.close(fd, function (err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                    });
+                } else if (err) {
+                    return console.error(err);
+                }
+
+                if (logRotation === 'd') {
+                    var today = (0, _moment2.default)((0, _moment2.default)(), 'M/D/YYYY');
+                    var fileDate = (0, _moment2.default)((0, _moment2.default)(stat.birthtime), 'M/D/YYYY');
+                    var diffHours = today.diff(fileDate, 'hours');
+
+                    if (parseInt(diffHours) - parseInt((0, _moment2.default)().startOf('day').fromNow()) > 1) {
+                        _fs2.default.rename(fileName, fileName + '.' + (0, _moment2.default)(stat.birthtime).format('MM_DD_YYYY'), function (err) {
+                            if (err) console.log('ERROR: ' + err);
+
+                            _fs2.default.open(fileName, 'a+', function (err, fd) {
+                                if (err) {
+                                    return console.error(err);
+                                }
+
+                                _fs2.default.close(fd, function (err) {
+                                    if (err) {
+                                        console.log(err);
+                                    }
+                                });
+                            });
+                        });
+                    }
                 }
             });
         }
--- src/index.js
+++ src/index.js
@@ -1,53 +1,131 @@
 import fs from 'fs';
+import moment from 'moment';
 
 class Logger {
-    constructor(fileName, ferq = 'd', size = 0) {
-        this.fileName = fileName;
+    // constructor() {}
 
-        if (ferq === 'd') {
-            fs.stat('input.txt', function (err, stats) {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log(stats);
-                console.log("Got file info successfully!");
+    constructor(logFileName, errorFileName, logLevel, logRotation, logSize, logJson, logTimeStamp) {
+        this.logFileName = logFileName;
+        this.errorFileName = errorFileName;
 
-                // Check file type
-                console.log("isFile ? " + stats.isFile());
-                console.log("isDirectory ? " + stats.isDirectory());
-            });
+        this.logLevel = logLevel;                         // Info
+        this.logRotation = logRotation;                      // Daily
+        this.logSize = logSize;      // 5 MB
+        this.logJson = logJson;                             // True
+        this.logTimeStamp = logTimeStamp;                   // True
+    }
 
+    static configure(properties) {
+        let logLevel = properties.level || 'info';                         // Info
+        let logRotation = properties.rotation || 'd';                      // Daily
+        let logSize = properties.size * 1048576 || 5242880;      // 5 MB
+        let logJson = properties.json || true;                             // True
+        let logTimeStamp = properties.timeStamp || true;
 
+        let logFileName = 'logs/app_log_current.log';
+        let errorFileName = 'logs/error_log_current.log';
 
-            fs.open(this.fileName, 'a+', function(err, fd) {
-                if (err) {
-                    return console.error(err);
-                }
-
-                fs.close(fd, function(err) {
-                    if (err){
-                        console.log(err);
-                    }
-                });
-            });
+        if (!fs.existsSync('logs')){
+            fs.mkdirSync('logs');
         }
+
+        this._createLogFile(logFileName, logRotation, logSize);
+        this._createLogFile(errorFileName, logRotation, logSize);
+
+        return new Logger(logFileName, errorFileName, logLevel, logRotation, logSize, logJson, logTimeStamp);
     }
 
-    debug(debugString, debugData) {
-        fs.appendFile(this.fileName, "DEBUG: " + debugString + " : " + debugData + '\r\n', function(err) {
-            if(err) {
+    static _createLogFile(fileName, logRotation, logSize) {
+        fs.stat(fileName, function (err, stat) {
+            if (err && err.code == 'ENOENT') {
+                fs.open(fileName, 'a+', function (err, fd) {
+                    if (err) {
+                        return console.error(err);
+                    }
+
+                    fs.close(fd, function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                });
+            } else if (err) {
+                return console.error(err);
+            }
+
+            if (logRotation === 'd') {
+                let today = moment(moment(), 'M/D/YYYY');
+                let fileDate = moment(moment(stat.birthtime), 'M/D/YYYY');
+                let diffHours = today.diff(fileDate, 'hours');
+
+                if (parseInt(diffHours) - parseInt((moment().startOf('day').fromNow())) > 1) {
+                    fs.rename(fileName, fileName + '.' + moment(stat.birthtime).format('MM_DD_YYYY'), function (err) {
+                        if (err) console.log('ERROR: ' + err);
+
+                        fs.open(fileName, 'a+', function (err, fd) {
+                            if (err) {
+                                return console.error(err);
+                            }
+
+                            fs.close(fd, function (err) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            });
+                        });
+                    });
+                }
+            }
+        });
+    }
+
+
+
+    info(infoMessage) {
+        let logData = {};
+        logData['LEVEL'] = this.logLevel;
+        logData['MESSAGE'] = infoMessage;
+        logData['TIMESTAMP'] = moment(moment(), 'M/D/YYYY');
+
+        fs.appendFile(this.logFileName, JSON.stringify(logData) + '\r\n', function (err) {
+            if (err) {
                 return console.log(err);
             }
         });
     }
 
-    info(infoString) {
-        fs.appendFile(this.fileName, "INFO: " + infoString  + '\r\n', function(err) {
-            if(err) {
+
+    debug(debugMessage, debugData) {
+        let logData = {};
+        logData['LEVEL'] = this.logLevel;
+        logData['MESSAGE'] = debugMessage;
+        logData['DETAILS'] = debugData;
+        logData['TIMESTAMP'] = moment(moment(), 'M/D/YYYY');
+
+        fs.appendFile(this.logFileName, JSON.stringify(logData) + '\r\n', function (err) {
+            if (err) {
                 return console.log(err);
             }
         });
     }
+
+
+
+    error(errorMessage, errorData) {
+        let logData = {};
+        logData['LEVEL'] = this.logLevel;
+        logData['MESSAGE'] = errorMessage;
+        logData['DETAILS'] = errorData;
+        logData['TIMESTAMP'] = moment(moment(), 'M/D/YYYY');
+
+        fs.appendFile(this.errorFileName, JSON.stringify(logData) + '\r\n', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
+
+
 }
 
 export default Logger;
